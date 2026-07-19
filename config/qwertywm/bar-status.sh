81f5efd194821d17975038227c78e3ee16c0bb71
ldgnu <ldgnu@users.noreply.github.com>
2026-07-19 02:02:23 -0300

    fix: correct paths for bar script and css

--- config/qwertywm/bar-status.sh
+++ config/qwertywm/bar-status.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 data=$(qwertywmctl get state 2>/dev/null)
-tags=$(echo "$data" | jq -r '[.outputs[] | if .focused then "[" + .workspace + "]" else .workspace end] | join(" ")' 2>/dev/null)
+w1=$(echo "$data" | jq -r '.outputs[0].workspace' 2>/dev/null)
+w2=$(echo "$data" | jq -r '.outputs[1].workspace' 2>/dev/null)
+foc=$(echo "$data" | jq -r '[.outputs[] | select(.focused) | .workspace][0]' 2>/dev/null)
 title=$(echo "$data" | jq -r '[.windows[] | select(.focused) | .title][0]' 2>/dev/null)
-echo '{"text":"'"$tags  $title"'"}'
+
+if [ "$foc" = "$w1" ]; then
+  echo "{\"text\":\"[$w1] $w2  $title\"}"
+else
+  echo "{\"text\":\"$w1 [$w2]  $title\"}"
+fi
