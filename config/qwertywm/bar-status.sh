e4e71ff00527b1859aeb845cb1c53e59b9c36663
ldgnu <ldgnu@users.noreply.github.com>
2026-07-19 01:54:51 -0300

    bar: show focused workspace with brackets

--- config/qwertywm/bar-status.sh
+++ config/qwertywm/bar-status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true; do
   data=$(qwertywmctl get state 2>/dev/null)
-  tags=$(echo "$data" | jq -r '[.outputs[] | .workspace] | join(" ")' 2>/dev/null)
+  tags=$(echo "$data" | jq -r '[.outputs[] | if .focused then "[" + .workspace + "]" else .workspace end] | join(" ")' 2>/dev/null)
   title=$(echo "$data" | jq -r '[.windows[] | select(.focused) | .title][0]' 2>/dev/null)
   echo '{"text":"'"$tags  $title"'"}'
   sleep 1
