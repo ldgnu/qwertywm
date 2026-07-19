feeac7650e1b5c13aba65781a3fd23d40a0faed7
ldgnu <ldgnu@users.noreply.github.com>
2026-07-19 01:58:38 -0300

    fix: bar script no loop, waybar manages interval

--- config/qwertywm/bar-status.sh
+++ config/qwertywm/bar-status.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-while true; do
-  data=$(qwertywmctl get state 2>/dev/null)
-  tags=$(echo "$data" | jq -r '[.outputs[] | if .focused then "[" + .workspace + "]" else .workspace end] | join(" ")' 2>/dev/null)
-  title=$(echo "$data" | jq -r '[.windows[] | select(.focused) | .title][0]' 2>/dev/null)
-  echo '{"text":"'"$tags  $title"'"}'
-  sleep 1
-done
+data=$(qwertywmctl get state 2>/dev/null)
+tags=$(echo "$data" | jq -r '[.outputs[] | if .focused then "[" + .workspace + "]" else .workspace end] | join(" ")' 2>/dev/null)
+title=$(echo "$data" | jq -r '[.windows[] | select(.focused) | .title][0]' 2>/dev/null)
+echo '{"text":"'"$tags  $title"'"}'
